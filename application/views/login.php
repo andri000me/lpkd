0e688f3e339de1cd5d306c13a7fa21ea65a34369
syamsul <asyamsul95@gmail.com>
2020-02-20 15:35:00 +0700

    TA Project login

--- application/views/login.php
+++ application/views/login.php
@@ -13,7 +13,7 @@
         <!-- iCheck -->
         <link href="<?php echo base_url('assets/AdminLTE-2.0.5/plugins/iCheck/square/blue.css') ?>" rel="stylesheet" type="text/css" />
 
-        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!-- HTML5 xxxxxxx Shim and Respond.js IE8 support of HTML5 elements and media queries -->
         <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
         <!--[if lt IE 9]>
             <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
